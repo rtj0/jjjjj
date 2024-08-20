6f33da3cf95be74332d34d6f896e1ad360c990e2
Raaid Taha <tahr475@student.otago.ac.nz>
2024-08-20 13:55:01 +1200

    j

---
+++ j.cpp
@@ -0,0 +1,21 @@
+#include "../helper/combat.cpp"
+#include "../helper/minigames.cpp"
+// test docs
+int main()
+{
+#if defined(_WIN32) || defined(_WIN64)
+  SetConsoleSize(800, 600);
+#else
+  SetTerminalSize(24, 80);
+#endif
+
+  // typePrint(getFileContent("../reasources/intro.txt"));
+  // combatV1(100, 100, 2000);
+  // TicTacToe game;
+  // game.startGame();
+
+  CodeGuesser ggame;
+  ggame.start();
+
+  return 0;
+}
